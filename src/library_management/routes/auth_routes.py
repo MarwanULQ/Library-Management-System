c29c7ee24628b42b9309589e610cc7e458c92591
Aki3006 <amr.120230092@ejust.edu.eg>
2025-12-18 23:55:15 +0200

    routes

---
+++ src/library_management/routes/auth_routes.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, HTTPException
+from services.auth_services import signup, login
+
+router = APIRouter(prefix="/auth")
+
+@router.post("/signup")
+def signup(email: str, password: str):
+    try:
+        user_id = signup(email, password)
+        return {"userId": user_id}
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+@router.post("/login")
+def login(email: str, password: str):
+    try:
+        user_id = login(email, password)
+        return {"userId": user_id}
+    except ValueError as e:
+        raise HTTPException(status_code=401, detail=str(e))
+    
