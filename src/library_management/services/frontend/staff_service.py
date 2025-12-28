5aa957b912306f4b27df33fc2a6708f853d54187
Aki3006 <amr.120230092@ejust.edu.eg>
2025-12-28 21:12:00 +0200

    Staff Service

---
+++ src/library_management/services/frontend/staff_service.py
@@ -0,0 +1,32 @@
+from models.staff_model import Staff, StaffRole
+from services.frontend.api_helper import ApiHelper
+
+class StudentService:
+
+    @staticmethod
+    def get_staff(staffId: int) -> Staff:
+
+        try:
+            response = ApiHelper.get(f"/db/staff/{staffId}")
+        except Exception as e:
+            raise Exception(f"An error occurred while fetching the staff: {e}")
+
+        if response.status_code != 200:
+            raise Exception(f"Failed to fetch staff: {response.json().get('detail')}")
+
+        return Staff.from_json(response.json())
+    
+    @staticmethod
+    def create_staff(staff: Staff) -> Staff:
+
+        try:
+            response = ApiHelper.post(f"/db/staff", data=Staff.to_json())
+        except Exception as e:
+            raise Exception(f"An error occurred while creating the staff: {e}")
+
+        if response.status_code != 200:
+            raise Exception(f"Failed to create staff: {response.json().get('detail')}")
+
+        return Staff.from_json(response.json())
+        
+    
