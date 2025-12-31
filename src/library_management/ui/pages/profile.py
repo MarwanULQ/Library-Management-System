e154c4e1890d762e77f469c3a1fc1afd6f71ee80
zainstark <yousef.2520092@gmail.com>
2025-12-31 02:06:31 +0200

    Built Staff Components + Used StudentID for Profile page

--- src/library_management/ui/pages/profile.py
+++ src/library_management/ui/pages/profile.py
@@ -8,7 +8,7 @@ from ui.components.profile_tabs import ProfileTabs
 user_data = {
     "name": st.session_state.email.split("@")[0].split(".")[0].capitalize(),
     "email": st.session_state.email,
-    "student_id": st.session_state.email.split("@")[0].split(".")[1]
+    "student_id": st.session_state.student_id
 }
 
 apply_global_styles()
