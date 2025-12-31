595c16213021a4116926f4d9615cc814a3d938d3
zainstark <yousef.2520092@gmail.com>
2025-12-31 04:06:39 +0200

    Integrating Staff stuff: Trial 1

--- src/library_management/ui/pages/staff.py
+++ src/library_management/ui/pages/staff.py
@@ -3,9 +3,7 @@ from ui.components.staff_header import StaffHeader
 from ui.components.staff_tabs import StaffTabs
 import streamlit as st
 
-if st.session_state.logged_in and st.session_state.role == "Staff":
-    apply_global_styles()
-    StaffHeader().render()
-    StaffTabs().render()
-else:
-    st.error("You must Sign in and better to be an admin to access staff dashboard")
+
+apply_global_styles()
+StaffHeader().render()
+StaffTabs().render()
